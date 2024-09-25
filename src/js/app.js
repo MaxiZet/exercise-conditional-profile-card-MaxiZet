5b7db8ad7fff36acc8b67ca8875522fa8c7bbbba
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-09-25 21:45:33 +0000

    Links Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -42,16 +42,16 @@ function render(variables = {}) {
     variables.country == null ? "" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${
+            <li><a href="https://x.com/${
               variables.twitter == null ? "" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${
+            <li><a href="https://github.com/${
               variables.github == null ? "" : variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="${
+            <li><a href="https://www.linkedin.com/in/${
               variables.linkedin == null ? "" : variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${
+            <li><a href="https://www.instagram.com/${
               variables.instagram == null ? "" : variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
